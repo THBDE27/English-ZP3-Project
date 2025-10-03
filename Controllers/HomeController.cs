75f51eacc5c9cc4793107dd878836f6157e5b97e
THBDE27 <eliethibodeau@outlook.com>
2025-10-03 19:41:00 -0400

    Revert "renamed index to intro"
    This reverts commit b0e0118422c3e900c801bd0a8df70ac4509f6da1.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,12 +14,20 @@ namespace English_ZP3_Project.Controllers
             _logger = logger;
         }
 
-        public IActionResult Intro()
+        public IActionResult Index()
         {
-            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files","intro");
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files");
 
-            ViewBag.EnText = System.IO.File.ReadAllText(folderPath + "en.txt");
-            ViewBag.FrText = System.IO.File.ReadAllText(folderPath + "fr.txt");
+            // Read both files
+            string enFile = Path.Combine(folderPath, "introen.txt");
+            string frFile = Path.Combine(folderPath, "introfr.txt");
+
+            string enText = System.IO.File.Exists(enFile) ? System.IO.File.ReadAllText(enFile) : "English file not found.";
+            string frText = System.IO.File.Exists(frFile) ? System.IO.File.ReadAllText(frFile) : "French file not found.";
+
+            // Pass to ViewBag
+            ViewBag.EnText = enText;
+            ViewBag.FrText = frText;
 
             return View();
         }
