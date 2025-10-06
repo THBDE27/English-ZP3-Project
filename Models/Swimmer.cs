f6d626436fdf9d535dd4c81a09286b24a51fd294
THBDE27 <eliethibodeau@outlook.com>
2025-10-06 19:50:19 -0400

    swimmer properties (birthday)

--- Models/Swimmer.cs
+++ Models/Swimmer.cs
@@ -1,48 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+﻿using System.Drawing;
 
 namespace English_ZP3_Project.Models
 {
     public class Swimmer
     {
-        public Swimmer(string first, string last)
+        public Swimmer(string first, string last, int birthDay, int month, int year)
         {
             FirstName = first;
             MiddleName = "";
             LastName = last;
+            Birthday = SetBirthday(birthDay, month, year);
+            Age = SetAge(birthDay, month, year);
         }
 
-        public Swimmer(string first, string middle, string last)
+        public Swimmer(string first, string middle, string last, int birthDay, int month, int year)
         {
             FirstName = first;
             MiddleName = middle;
             LastName = last;
+            Birthday = SetBirthday(birthDay, month, year);
+            Age = SetAge(birthDay, month, year);
         }
 
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; }
+        public string MiddleName { get; }
+        public string LastName { get; }
         public string Name
         {
             get
             {
-                return FirstName + " " + MiddleName + " " + LastName;
+                if (MiddleName == null)
+                {
+                    return FirstName + " " + LastName;
+                }
+                else
+                {
+                    return FirstName + " " + MiddleName + " " + LastName;
+                }
             }
         }
+        public string Initials { get => FirstName[0] + "" + MiddleName[0] + "" + LastName[0]; }
 
-
-        public string Initials
+        public string Birthday { get; }
+        static string SetBirthday(int birthDate, int month, int year)
         {
-            get
+            string date = "";
+
+            if (birthDate < 10)
             {
-                return FirstName[0] + "" + MiddleName[0] + "" + LastName[0];
+                date += "0";
             }
+
+            date += birthDate + ".";
+
+            if (month < 10)
+            {
+                date += 0;
+            }
+
+            date += month + ".";
+            date += year.ToString()[2] + year.ToString()[3];
+
+            return date;
+        }
+
+        public int Age { get; }
+        static int SetAge(int birthDate, int month, int year)
+        {
+            DateTime today = DateTime.Today;
+
+            int age = today.Year - year;
+
+            if (today.Month < month || (today.Month == month && today.Day < birthDate))
+            {
+                age--;
+            }
+
+            return age;
         }
 
         public string Description
         {
             get
             {
-                string file = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", "biographies", Initials + ".txt");
+                string file = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", "biographies", Initials + Birthday + ".txt");
                 return System.IO.File.ReadAllText(file);
             }
         }
