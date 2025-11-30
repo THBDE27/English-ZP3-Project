41044453f3cde559bcffc9c7869ee69ce214a12e
THBDE27 <eliethibodeau@outlook.com>
2025-11-29 19:35:50 -0500

    Revert "glossary now works on published... sizing is tweaking tho"
    This reverts commit a1e4eea65dd689ae0f3bca5bb8ff30083033b0ea.

--- Helpers/FileHelper.cs
+++ Helpers/FileHelper.cs
@@ -2,20 +2,27 @@
 
 namespace English_ZP3_Project.Helpers
 {
-    public class FileHelper(IWebHostEnvironment env)
+    public class FileHelper
     {
-        private readonly string _webRootPath = env.WebRootPath;
+        private readonly string _webRootPath;
+
+        public FileHelper(IWebHostEnvironment env)
+        {
+            _webRootPath = env.WebRootPath;
+        }
+
 
         public List<string> GetFileNames(string relativeFolderPath, string extension)
         {
             string startFolder = Path.Combine(_webRootPath, relativeFolderPath);
 
             if (!Directory.Exists(startFolder))
-                return [];
+                return new List<string>();
 
-            List<string> allFolders = [.. Directory.GetFiles(startFolder, "*" + extension, SearchOption.AllDirectories)];
+            List<string> allFolders = new List<string>();
+            allFolders.AddRange(Directory.GetFiles(startFolder, "*" + extension, SearchOption.AllDirectories));
 
-            List<string> names = [];
+            List<string> names = new List<string>();
             foreach (var folder in allFolders)
             {
                 names.Add(Path.GetFileName(folder));
@@ -26,7 +33,7 @@ namespace English_ZP3_Project.Helpers
 
         public List<string> GetVideoSources(string folderPath)
         {
-            List<string> srcs = [];
+            List<string> srcs = new();
            
             foreach (string name in GetFileNames(folderPath, "mp4"))
             {
--- Helpers/FolderHelper.cs
+++ Helpers/FolderHelper.cs
@@ -5,20 +5,27 @@
     using System.IO;
     using static NuGet.Packaging.PackagingConstants;
 
-    public class FolderHelper(IWebHostEnvironment env)
+    public class FolderHelper
     {
-        private readonly string _webRootPath = env.WebRootPath;
+        private readonly string _webRootPath;
 
+        public FolderHelper(IWebHostEnvironment env)
+        {
+            _webRootPath = env.WebRootPath;
+        }
+
+       
         public List<string> GetSubFolderNames(string relativeFolderPath)
         {
             string startFolder = Path.Combine(_webRootPath, relativeFolderPath);
 
             if (!Directory.Exists(startFolder))
-                return []; 
+                return new List<string>(); 
 
-            List<string> allFolders = [.. Directory.GetDirectories(startFolder, "*", SearchOption.AllDirectories)];
+            List<string> allFolders = new List<string> ();
+            allFolders.AddRange(Directory.GetDirectories(startFolder, "*", SearchOption.AllDirectories));
 
-            List<string> names = [];
+            List<string> names = new List<string>();
             foreach (var folder in allFolders)
             {
                 names.Add(Path.GetFileName(folder));
